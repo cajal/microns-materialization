2501eb2983a73916ec8003e9a64b30d2ee34b557
spapa013 <stelios@spapa.us>
2021-10-26 10:08:23 -0500

    updates setup.py

--- python/setup.py
+++ python/setup.py
@@ -8,9 +8,9 @@ here = path.abspath(path.dirname(__file__))
 setup(
     name='microns_materialization',
     version='0.0.1',
-    description='Datajoint schemas for the MICrONS project',
+    description='Datajoint schemas for importing external data for the MICrONS project',
     author='Stelios Papadopoulos',
     author_email='spapadop@bcm.edu',
     packages=find_packages(exclude=[]),
-    install_requires=['numpy', 'pandas', 'scipy', 'datajoint==0.12.9', 'decorator', 'matplotlib', 'caveclient', 'nglui', 'tqdm']
+    install_requires=['numpy', 'pandas', 'scipy', 'decorator', 'matplotlib', 'caveclient', 'nglui', 'tqdm']
 )
