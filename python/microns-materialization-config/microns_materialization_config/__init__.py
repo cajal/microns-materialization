6c1e8a1010500729bcab97f92f4ca2b38307fae3
root <celiibrendan@gmail.com>
2021-11-08 20:17:02 +0000

    removed external/adapter mappings

--- python/microns-materialization-config/microns_materialization_config/__init__.py
+++ python/microns-materialization-config/microns_materialization_config/__init__.py
@@ -1,5 +1,5 @@
 """
-Configuration package/module for microns-MATERIALIZATION.
+Configuration package/module for microns-materialization.
 """
 
 from . import adapters
@@ -13,77 +13,40 @@ except:
     traceback.print_exc()
     raise ImportError('DataJoint package not found.')
 
-# Also important to run the dj flags at SOME point during normal datajoint initialization, probably don't want to have this in every config though?
-
-# TODO: place in microns-utils
-def enable_datajoint_flags(enable_python_native_blobs=True):
-    """
-    Enable experimental datajoint features
-    
-    These flags are required by 0.12.0+ (for now).
-    """
-    dj.config['enable_python_native_blobs'] = enable_python_native_blobs
-    dj.errors._switch_filepath_types(True)
-    dj.errors._switch_adapted_types(True)
-    
-enable_datajoint_flags()
-
-# IMPORTANT: You can organize several schemas' config files (or folders) however you want as long as
-# the correct schema configurations are plugged in for the enum variations (obviously).
-
 from enum import Enum
+
+from microns_utils import config_utils
+    
+config_utils.enable_datajoint_flags()
+
+def register_externals(schema_name:str):
+    """
+    Registers the external stores for a schema_name in this module.
+    """
+    return config_utils.register_externals(config_mapping[SCHEMAS(schema_name)]["externals"])
+
+
+def register_adapters(schema_name:str, context=None):
+    """
+    Imports the adapters for a schema_name into the global namespace.
+    """
+    #return config_utils.register_adapters(adapters_mapping[schema_name], context=context)
+    return config_utils.register_adapters(config_mapping[SCHEMAS(schema_name)]["adapters"], context=context)
+
+
+def create_vm(schema_name:str):
+    """
+    Creates a virtual module after registering the external stores, and includes the adapter objects in the vm.
+    """
+    schema = SCHEMAS(schema_name)
+    return config_utils.create_vm(schema.value, 
+                                  external_stores=config_mapping[schema]["externals"],
+                                  adapter_objects=config_mapping[schema]["adapters"])
+
 
 class SCHEMAS(Enum):
     H01_MATERIALIZATION = "microns_h01_materialization"
     MINNIE65_MATERIALIZATION = "microns_minnie65_materialization"
-
-
-
-def register_externals(schema_name:str=None,
-                       stores_config:dict=None):
-    """
-    Registers the external stores for a schema in this module.
-    """
-    
-    if stores_config is None and schema_name is None:
-        raise Exception("Either stores_config or schema_name must be set")
-    
-    if schema_name is not None: 
-        stores_config = externals_mapping[schema_name]
-    
-    if 'stores' not in dj.config:
-        dj.config['stores'] = stores_config
-    else:
-        dj.config['stores'].update(stores_config)
-
-
-def register_adapters(
-    schema_name:str=None,
-      adapter_objects:dict=None, 
-      context=None):
-    """
-    Imports the adapters for a schema into the global namespace.
-    
-    This function is probably not necessary, but standardization is nice.
-    """
-    if adapter_objects is None and schema_name is None:
-        raise Exception("Either adapter_objects or schema_name must be set")
-    if schema_name is not None: 
-        adapter_objects = adapters_mapping[schema_name]
-    
-    if context is None:
-        # if context is missing, use the calling namespace
-        import inspect
-        frame = inspect.currentframe().f_back
-        context = frame.f_locals
-        del frame
-    
-    for name, adapter in adapter_objects.items():
-        context[name] = adapter
-
-
-# Typing annotation hints not strictly necessary. This import is also not necessary if you only specify one type.
-from typing import Union
 
 
 config_mapping = {
@@ -97,28 +60,3 @@ config_mapping = {
     },
     
 }
-
-adapters_mapping = {
-    "microns_h01_materialization":config_mapping[SCHEMAS.H01_MATERIALIZATION]["adapters"],
-    "microns_minnie65_materialization":config_mapping[SCHEMAS.MINNIE65_MATERIALIZATION]["adapters"],
-}
-
-externals_mapping = {
-    "microns_h01_materialization":config_mapping[SCHEMAS.H01_MATERIALIZATION]["externals"],
-    "microns_minnie65_materialization":config_mapping[SCHEMAS.MINNIE65_MATERIALIZATION]["externals"],
-}
-
-
-def create_vm(schema_name:str):
-    """
-    Creates a virtual module after registering the external stores, and includes the adapter objects in the vm.
-    """
-    
-    # Steps that create_vm should take for each schema:
-    # 1. Register externals with dj.config
-    # 2. Choose which schema's config to load.
-    # 3. Load a dict with the relevant adapters into the adapter object field of a virtual module.
-    if externals is not None:
-        register_externals(schema_name)
-    
-    return dj.create_virtual_module(schema_name, schema_name, add_objects=adapters_mapping[schema_name])
