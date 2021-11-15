84c83afc8c2fd2f02f9d32474b3ae28690488d19
spapa013 <stelios@spapa.us>
2021-11-14 21:32:46 -0600

    update directory and add __init__.py

--- python/microns-materialization/microns_materialization/__init__.py
+++ python/microns-materialization/microns_materialization/__init__.py
@@ -1,6 +1,6 @@
 from microns_utils import config_utils
 
 _repo = 'microns-materialization'
-_package = 'microns-materialization-api'
+_package = _repo
 
 __version__ = config_utils.get_package_version(repo=_repo, package=_package)
