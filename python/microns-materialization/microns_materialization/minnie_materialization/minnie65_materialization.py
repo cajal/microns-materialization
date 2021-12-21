411f4ff56cd43ffafaef7a9f01a2866d2994730a
spapa013 <stelios@spapa.us>
2021-12-21 14:50:46 -0600

    move definitions to api.schemas

--- python/microns-materialization/microns_materialization/minnie_materialization/minnie65_materialization.py
+++ python/microns-materialization/microns_materialization/minnie_materialization/minnie65_materialization.py
@@ -17,43 +17,13 @@ else:
     from tqdm import tqdm
 
 # Schema creation
-from microns_materialization_api import config
-schema_obj = config.SCHEMAS.MINNIE65_MATERIALIZATION
+from microns_materialization_api.schemas import minnie65_materialization as m65mat
 
-config.register_adapters(schema_obj, context=locals())
-config.register_externals(schema_obj)
-
-# Schema creation
-schema = dj.schema(schema_obj.value)
-schema.spawn_missing_classes()
-schema.connection.dependencies.load()
-
-@schema
-class Materialization(dj.Manual):
-    definition = """
-    # version and timestamp of Minnie65 materialization
-    ver              : DECIMAL(6,2)    # materialization version
-    ---
-    valid=1          : tinyint         # marks whether the materialization is valid. Defaults to 1.
-    timestamp        : timestamp       # marks the time at which the materialization service was started
-    """
+class Materialization(m65mat.Materialization):
     
-    class CurrentVersion(dj.Part):
-        definition = """
-        # version and timestamp of Minnie65 materialization
-        kind       :  varchar(16)        # selection criteria for setting the current materialization
-        ---
-        -> master
-        description=NULL                  : varchar(256)      # description of the materialization and source
-        """    
+    class CurrentVersion(m65mat.Materialization.CurrentVersion): pass   
     
-    class Meta(dj.Part):
-        definition = """
-        -> master
-        ---
-        description=NULL                  : varchar(256)      # description of the materialization and source
-        ts_inserted=CURRENT_TIMESTAMP     : timestamp         # timestamp that data was inserted into this DataJoint table.
-        """
+    class Meta(m65mat.Materialization.Meta): pass
 
     @classmethod
     def fill(cls, client, ver, description=None, update_latest_tag=True):
@@ -82,11 +52,11 @@ class Materialization(dj.Manual):
                     print(f'Updated Materialization.CurrentVersion "latest" to materialization version {client.materialize.version:.2f}')
 
 
-stable = Materialization.CurrentVersion & {'kind': 'stable'}
-latest = Materialization.CurrentVersion & {'kind': 'latest'}
-MICrONS_Mar_2021 = Materialization.CurrentVersion & {'kind': 'MICrONS_Mar_2021'}
-MICrONS_Jul_2021 = Materialization.CurrentVersion & {'kind': 'MICrONS_Jul_2021'}
-releaseJun2021 = Materialization.CurrentVersion & {'kind': 'release_Jun2021'}
+stable = m65mat.stable
+latest = m65mat.latest
+MICrONS_Mar_2021 = m65mat.MICrONS_Mar_2021
+MICrONS_Jul_2021 = m65mat.MICrONS_Jul_2021
+releaseJun2021 = m65mat.releaseJun2021
 
 def _version_mgr(client, ver, table=Materialization):
     """ Modify the client to the user specified materialization version and check if the version is already in the DataJoint table. 
@@ -127,35 +97,12 @@ def _version_mgr(client, ver, table=Materialization):
         return code, client
 
 
-@schema
-class Nucleus(dj.Manual):
-    definition = """
-    # Nucleus detection from version 0 of the Allen Institute. Table name: 'nucleus_detection_v0'
-    nucleus_id   : int unsigned   # id of nucleus from the flat segmentation  Equivalent to Allen: 'id'.
-    ---
-    """
+
+class Nucleus(m65mat.Nucleus):
     
-    class Info(dj.Part):
-        definition = """
-        # Detailed information from each nucleus_id
-        -> Materialization
-        -> master     
-        segment_id       : bigint unsigned    # id of the segment under the nucleus centroid. Equivalent to Allen 'pt_root_id'.
-        ---
-        nucleus_x        : int unsigned       # x coordinate of nucleus centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-        nucleus_y        : int unsigned       # y coordinate of nucleus centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-        nucleus_z        : int unsigned       # z coordinate of nucleus centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-        supervoxel_id    : bigint unsigned    # id of the supervoxel under the nucleus centroid. Equivalent to Allen: 'pt_supervoxel_id'.
-        volume=NULL      : float              # volume of the nucleus in um^3
-        """
+    class Info(m65mat.Nucleus.Info): pass
         
-    class Meta(dj.Part):
-        definition = """
-        ->Materialization
-        ---
-        description=NULL                  : varchar(256)      # description of the table version
-        ts_inserted=CURRENT_TIMESTAMP     : timestamp         # timestamp that data was inserted into this DataJoint table.
-        """
+    class Meta(m65mat.Nucleus.Meta): pass
     
     @classmethod
     def fetch_df(cls, client):
@@ -201,30 +148,10 @@ class Nucleus(dj.Manual):
         print(f'Successfully inserted nucleus information for materialization version: {client.materialize.version:.2f}')
 
 
-@schema
-class FunctionalCoreg(dj.Manual):
-    definition = """
-    # ID's of cells from the table 'functional_coreg'
-    ->Materialization
-    ->Nucleus
-    segment_id       : bigint unsigned    # id of the segment under the nucleus centroid. Equivalent to Allen 'pt_root_id'.
-    scan_session     : smallint           # session index for the mouse 
-    scan_idx         : smallint           # number of TIFF stack file
-    unit_id          : int                # unit id from ScanSet.Unit
-    ---
-    centroid_x       : int unsigned       # x coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    centroid_y       : int unsigned       # y coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    centroid_z       : int unsigned       # z coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    supervoxel_id    : bigint unsigned    # id of the supervoxel under the nucleus centroid. Equivalent to Allen: 'pt_supervoxel_id'.
-    """
+
+class FunctionalCoreg(m65mat.FunctionalCoreg):
         
-    class Meta(dj.Part):
-        definition = """
-        ->Materialization
-        ---
-        description=NULL                  : varchar(256)      # description of the table version
-        ts_inserted=CURRENT_TIMESTAMP     : timestamp         # timestamp that data was inserted into this DataJoint table.
-        """   
+    class Meta(m65mat.FunctionalCoreg.Meta): pass
     
     @classmethod
     def fetch_df(cls, client):
@@ -264,21 +191,9 @@ class FunctionalCoreg(dj.Manual):
         print(f'Successfully inserted functional coregistration for materialization version: {client.materialize.version:.2f}')
 
 
-@schema
-class ProofreadSegment(dj.Manual):
-    definition = """
-    # Segment ID's of manually proofread neurons from 'proofreading_functional_coreg_v2'
-    ->Materialization
-    segment_id       : bigint unsigned    # id of the segment under the nucleus centroid. Equivalent to Allen 'pt_root_id'.
-    """
+class ProofreadSegment(m65mat.ProofreadSegment):
 
-    class Meta(dj.Part):
-        definition = """
-        ->Materialization
-        ---
-        description=NULL                  : varchar(256)      # description of the table version
-        ts_inserted=CURRENT_TIMESTAMP     : timestamp         # timestamp that data was inserted into this DataJoint table.
-        """ 
+    class Meta(m65mat.ProofreadSegment): pass
 
     @classmethod
     def fetch_df(cls, client):
@@ -305,30 +220,9 @@ class ProofreadSegment(dj.Manual):
         print(f'Successfully inserted proofread segments for materialization version: {client.materialize.version:.2f}')
 
 
-@schema
-class ProofreadFunctionalCoregV2(dj.Manual):
-    definition = """
-    # ID's of cells from the table 'proofreading_functional_coreg_v2'
-    ->Materialization
-    ->Nucleus
-    segment_id       : bigint unsigned    # id of the segment under the nucleus centroid. Equivalent to Allen 'pt_root_id'.
-    scan_session     : smallint           # session index for the mouse 
-    scan_idx         : smallint           # number of TIFF stack file
-    unit_id          : int                # unit id from ScanSet.Unit
-    ---
-    centroid_x       : int unsigned       # x coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    centroid_y       : int unsigned       # y coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    centroid_z       : int unsigned       # z coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    supervoxel_id    : bigint unsigned    # id of the supervoxel under the nucleus centroid. Equivalent to Allen: 'pt_supervoxel_id'.
-    """
+class ProofreadFunctionalCoregV2(m65mat.ProofreadFunctionalCoregV2):
         
-    class Meta(dj.Part):
-        definition = """
-        ->Materialization
-        ---
-        description=NULL                  : varchar(256)      # description of the table version
-        ts_inserted=CURRENT_TIMESTAMP     : timestamp         # timestamp that data was inserted into this DataJoint table.
-        """ 
+    class Meta(m65mat.ProofreadFunctionalCoregV2.Meta): pass
     
     @classmethod
     def fetch_df(cls, client):
@@ -353,30 +247,10 @@ class ProofreadFunctionalCoregV2(dj.Manual):
         return fc_nuc_df
 
 
-@schema
-class SynapseSegmentSource(dj.Manual):
-    definition = """
-    segment_id           : bigint unsigned              # id of the segment under the nucleus centroid. Equivalent to Allen 'pt_root_id'.
-    ---
-    include=1            : tinyint                      # 1 if included in synapse source, 0 if excluded from synapse source
-    """
+class SynapseSegmentSource(m65mat.SynapseSegmentSource): pass
 
 
-@schema
-class Synapse(dj.Computed):
-    definition = """
-    # Synapses from the table 'synapses_pni_2'
-    ->SynapseSegmentSource.proj(primary_seg_id='segment_id')
-    secondary_seg_id                              : bigint unsigned              # id of the segment that is synaptically paired to primary_segment_id.
-    synapse_id                                    : bigint unsigned              # synapse index within the segmentation
-    ---
-    prepost                                       : varchar(16)                  # whether the primary_seg_id is "presyn" or "postsyn"
-    synapse_x                                     : int unsigned                 # x coordinate of synapse centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm). From Allen 'ctr_pt_position'.
-    synapse_y                                     : int unsigned                 # y coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm). From Allen 'ctr_pt_position'.
-    synapse_z                                     : int unsigned                 # z coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm). From Allen 'ctr_pt_position'.
-    synapse_size                                  : int unsigned                 # (EM voxels) scaled by (4x4x40)
-    ts_inserted=CURRENT_TIMESTAMP                 : timestamp                    # timestamp that data was inserted into this DataJoint table.
-    """
+class Synapse(m65mat.Synapse):
     
     client = None
     
@@ -493,31 +367,9 @@ def fetch_materialization(ver=-1, update_latest_tag=True):
 
 ## ADDITIONAL TABLES
 
-@schema
-class AllenV1ColumnTypesSlanted(dj.Manual):
-    definition = """
-    # ID's of cells from the table 'allen_v1_column_types_slanted'
-    ->Materialization
-    ->Nucleus
-    segment_id                : bigint unsigned    # id of the segment under the nucleus centroid. Equivalent to Allen 'pt_root_id'.
-    ---
-    valid                     : tinyint            # 1 = valid entry, 0 = invalid entry
-    classification_system     : varchar(128)       # method for classification
-    cell_type                 : varchar(128)       # manually classified cell type
-    n_nuc                     : smallint           # number of nuclei associated with segment_id (single somas n_nuc = 1)
-    centroid_x                : int unsigned       # x coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    centroid_y                : int unsigned       # y coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    centroid_z                : int unsigned       # z coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    supervoxel_id             : bigint unsigned    # id of the supervoxel under the nucleus centroid. Equivalent to Allen: 'pt_supervoxel_id'.
-    """
+class AllenV1ColumnTypesSlanted(m65mat.AllenV1ColumnTypesSlanted):
         
-    class Meta(dj.Part):
-        definition = """
-        ->Materialization
-        ---
-        description=NULL                  : varchar(256)      # description of the table version
-        ts_inserted=CURRENT_TIMESTAMP     : timestamp         # timestamp that data was inserted into this DataJoint table.
-        """ 
+    class Meta(m65mat.AllenV1ColumnTypesSlanted.Meta): pass
     
     @classmethod
     def fetch_df(cls, client):
@@ -560,31 +412,9 @@ class AllenV1ColumnTypesSlanted(dj.Manual):
         print(f'Successfully inserted data for materialization version: {client.materialize.version:.2f}')
 
 
-@schema
-class AllenSomaCourseCellClassModelV1(dj.Manual):
-    definition = """
-    # ID's of cells from the table 'allen_soma_coarse_cell_class_model_v1'
-    ->Materialization
-    ->Nucleus
-    segment_id                : bigint unsigned    # id of the segment under the nucleus centroid. Equivalent to Allen 'pt_root_id'.
-    ---
-    valid                     : tinyint            # 1 = valid entry, 0 = invalid entry
-    classification_system     : varchar(128)       # method for classification
-    cell_type                 : varchar(128)       # manually classified cell type
-    n_nuc                     : smallint           # number of nuclei associated with segment_id (single somas n_nuc = 1)
-    centroid_x                : int unsigned       # x coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    centroid_y                : int unsigned       # y coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    centroid_z                : int unsigned       # z coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    supervoxel_id             : bigint unsigned    # id of the supervoxel under the nucleus centroid. Equivalent to Allen: 'pt_supervoxel_id'.
-    """
+class AllenSomaCourseCellClassModelV1(m65mat.AllenSomaCourseCellClassModelV1):
         
-    class Meta(dj.Part):
-        definition = """
-        ->Materialization
-        ---
-        description=NULL                  : varchar(256)      # description of the table version
-        ts_inserted=CURRENT_TIMESTAMP     : timestamp         # timestamp that data was inserted into this DataJoint table.
-        """ 
+    class Meta(m65mat.AllenSomaCourseCellClassModelV1.Meta): pass
     
     @classmethod
     def fetch_df(cls, client):
@@ -627,31 +457,9 @@ class AllenSomaCourseCellClassModelV1(dj.Manual):
         print(f'Successfully inserted data for materialization version: {client.materialize.version:.2f}')
 
 
-@schema
-class AllenSomaCourseCellClassModelV2(dj.Manual):
-    definition = """
-    # ID's of cells from the table 'allen_soma_coarse_cell_class_model_v2'
-    ->Materialization
-    ->Nucleus
-    segment_id                : bigint unsigned    # id of the segment under the nucleus centroid. Equivalent to Allen 'pt_root_id'.
-    ---
-    valid                     : tinyint            # 1 = valid entry, 0 = invalid entry
-    classification_system     : varchar(128)       # method for classification
-    cell_type                 : varchar(128)       # manually classified cell type
-    n_nuc                     : smallint           # number of nuclei associated with segment_id (single somas n_nuc = 1)
-    centroid_x                : int unsigned       # x coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    centroid_y                : int unsigned       # y coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    centroid_z                : int unsigned       # z coordinate of centroid in EM voxels (x: 4nm, y: 4nm, z: 40nm)
-    supervoxel_id             : bigint unsigned    # id of the supervoxel under the nucleus centroid. Equivalent to Allen: 'pt_supervoxel_id'.
-    """
+class AllenSomaCourseCellClassModelV2(m65mat.AllenSomaCourseCellClassModelV2):
         
-    class Meta(dj.Part):
-        definition = """
-        ->Materialization
-        ---
-        description=NULL                  : varchar(256)      # description of the table version
-        ts_inserted=CURRENT_TIMESTAMP     : timestamp         # timestamp that data was inserted into this DataJoint table.
-        """ 
+    class Meta(m65mat.AllenSomaCourseCellClassModelV2.Meta): pass
     
     @classmethod
     def fetch_df(cls, client):
