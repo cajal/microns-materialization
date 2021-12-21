4ee1c814fcd0d5b106a9439e8a69df0242110d30
spapa013 <stelios@spapa.us>
2021-12-21 14:51:24 -0600

    add h01_materialization to api.schemas and change import to reflect new structure

--- python/microns-materialization/microns_materialization/h01_materialization/h01_materialization.py
+++ python/microns-materialization/microns_materialization/h01_materialization/h01_materialization.py
@@ -16,13 +16,4 @@ if 'ipykernel' in sys.modules:
 else:
     from tqdm import tqdm
 
-from microns_materialization_api import config
-schema_obj = config.SCHEMAS.H01_MATERIALIZATION
-
-config.register_adapters(schema_obj, context=locals())
-config.register_externals(schema_obj)
-
-# Schema creation
-schema = dj.schema(schema_obj.value)
-schema.spawn_missing_classes()
-schema.connection.dependencies.load()
+from microns_materialization_api.schemas import h01_materialization as h01mat
