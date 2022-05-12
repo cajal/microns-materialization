1d74dec41c2275b0c540ddf445a296768aad1dca
spapa013 <stelios@spapa.us>
2022-05-12 10:46:13 -0500

    fix mat scripts

--- deploy/kubernetes/minnie65_download_meshes.py
+++ deploy/kubernetes/minnie65_download_meshes.py
@@ -1,5 +1,5 @@
 import os
 
-if __name__ == 'main':
+if __name__ == '__main__':
     from microns_materialization.minnie_materialization.minnie65_materialization import download_materialization
-    download_materialization(ver=int(os.getenv('MICRONS_MAT_VER_TO_DL')), download_meshes=True, download_synapses=False, loglevel=os.getenv('MICRONS_LOGLEVEL'))
+    download_materialization(ver=os.getenv('MICRONS_MAT_VER_TO_DL'), download_meshes=True, download_synapses=False, loglevel=os.getenv('MICRONS_LOGLEVEL'))
--- deploy/kubernetes/minnie65_download_pcg_skeletons.py
+++ deploy/kubernetes/minnie65_download_pcg_skeletons.py
@@ -1,5 +1,5 @@
 import os
 
-if __name__ == 'main':
+if __name__ == '__main__':
     from microns_materialization.minnie_materialization.minnie65_materialization import Queue, download_pcg_skeletons
     download_pcg_skeletons(Queue.PCGSkeleton, loglevel=os.getenv('MICRONS_LOGLEVEL'))
