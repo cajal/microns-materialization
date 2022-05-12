5e8c93f483c65fbe25d770b85feca973ea5c425e
spapa013 <stelios@spapa.us>
2022-05-12 06:24:17 -0500

    adds download scripts

---
+++ deploy/kubernetes/minnie65_download_synapses.py
@@ -0,0 +1,3 @@
+if __name__ == 'main':
+    from microns_materialization.minnie_materialization.minnie65_materialization import Queue, download_materialization
+    download_materialization(queue=Queue.Materialization, download_meshes=False, download_synapses=True)
