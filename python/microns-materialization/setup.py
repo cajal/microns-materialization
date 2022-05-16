2e630f9f46c043b4c10c12c769fb12c399161b38
spapa013 <stelios@spapa.us>
2022-05-16 03:44:27 -0500

    update setup.py to add list around api import

--- python/microns-materialization/setup.py
+++ python/microns-materialization/setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, '..', 'version.py')) as f:
 with open(path.join(here, 'requirements.txt')) as f:
     requirements = f.read().split()
 
-requirements += find_api('microns-materialization-api')
+requirements += [find_api('microns-materialization-api')]
 
 setup(
     name='microns_materialization',
